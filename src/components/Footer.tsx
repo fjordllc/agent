d9dcdef5427ea262d1ec37d12e512f40665fe4dc
kazuma-naka <kazuma.naka.01@gmail.com>
2024-12-11 12:24:17 -0500

    Footer 内、Copyrright 上部にリストを設置
    プライバシーポリシーと利用規約をテストとして配置

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,32 @@
 export default function Footer() {
+  const footerItems = [
+    { text: "プライバシーポリシー", url: "/privacy-policy" },
+    { text: "利用規約", url: "/tos" },
+  ];
   return (
     <footer className="footer footer-center bg-base-300 text-base-content p-4">
+      <ul
+        style={{
+          listStyle: "none",
+          padding: 0,
+          margin: 0,
+          display: "flex",
+          justifyContent: "center",
+          gap: "15px",
+        }}
+      >
+        {footerItems.map((item, index) => (
+          <li key={index} style={{ margin: "0" }}>
+            <a
+              href={item.url}
+              style={{ textDecoration: "none", color: "#007bff" }}
+            >
+              {item.text}
+            </a>
+          </li>
+        ))}
+      </ul>
+
       <aside>
         <p>
           Copyright © {new Date().getFullYear()} - All right reserved by Lokka,
