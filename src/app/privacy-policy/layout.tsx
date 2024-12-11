bfd9d821ffc8d3e30068281d9d8ec4c667e53bc6
kazuma-naka <kazuma.naka.01@gmail.com>
2024-12-11 12:41:20 -0500

    プライバシーポリシー page.tsx, layout.tsx の作成

---
+++ src/app/privacy-policy/layout.tsx
@@ -0,0 +1,9 @@
+import SingleLayout from "@components/layouts/SingleLayout";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return SingleLayout({ children });
+}
