fefa9280e05160d1dce46bd3f405e704707515ce
kazuma-naka <kazuma.naka.01@gmail.com>
2024-12-12 12:20:38 -0500

    docs テーブルのため、 database.types.ts を更新する

--- src/lib/database.types.ts
+++ src/lib/database.types.ts
@@ -4,44 +4,70 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json }
-  | Json[]
+  | Json[];
 
 export interface Database {
   public: {
     Tables: {
       companies: {
         Row: {
-          id: number
-          name: string
-          website: string | null
-          memo: string | null
-          created_at: string
-        }
+          id: number;
+          name: string;
+          website: string | null;
+          memo: string | null;
+          created_at: string;
+        };
         Insert: {
-          id?: number
-          name: string
-          website?: string | null
-          memo?: string | null
-          created_at?: string
-        }
+          id?: number;
+          name: string;
+          website?: string | null;
+          memo?: string | null;
+          created_at?: string;
+        };
         Update: {
-          id?: number
-          name?: string
-          website?: string | null
-          memo?: string | null
-          created_at?: string
-        }
-      }
-    }
+          id?: number;
+          name?: string;
+          website?: string | null;
+          memo?: string | null;
+          created_at?: string;
+        };
+      };
+      docs: {
+        Row: {
+          id: number;
+          title: string;
+          body: string;
+          created_at: string;
+          updated_at: string;
+          user_id: number;
+          last_updated_user_id: number;
+        };
+        Insert: {
+          title: string;
+          body: string;
+          created_at?: string;
+          updated_at?: string;
+          user_id: number;
+          last_updated_user_id: number;
+        };
+        Update: {
+          title?: string;
+          body?: string;
+          created_at?: string;
+          updated_at?: string;
+          user_id?: number;
+          last_updated_user_id?: number;
+        };
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
-
