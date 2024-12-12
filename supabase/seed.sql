96c59cdf13b5acdbff42659eea8ea7ad17d51b96
komagata <komagata@gmail.com>
2024-12-13 04:14:41 +0900

    userのseedの追加
    それと既存のmigrationのSQLをシンプルも化した。

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,4 +1,11 @@
+INSERT INTO auth.users (id, email, encrypted_password, created_at, updated_at)
+VALUES 
+  (gen_random_uuid(), 'admin@example.com', crypt('testtest', gen_salt('bf')), NOW(), NOW()),
+  (gen_random_uuid(), 'user1@example.com', crypt('testtest', gen_salt('bf')), NOW(), NOW());
+
 INSERT INTO companies (name, website, memo) VALUES ('株式会社ロッカ', 'https://lokka.jp', '良い会社');
 INSERT INTO companies (name, website, memo) VALUES ('株式会社リンゴ', 'https://example.com/apple', 'リンゴは良い会社');
 INSERT INTO companies (name, website, memo) VALUES ('株式会社バナナ', 'https://example.com/banana', 'バナナは良い会社');
 INSERT INTO companies (name, website, memo) VALUES ('株式会社オレンジ', 'https://example.com/orange', 'オレンジは良い会社');
+
+
