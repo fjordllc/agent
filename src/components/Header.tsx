49480fa812f51f751189ee5b0d345f9db5973751
komagata <komagata@gmail.com>
2024-12-14 22:45:54 +0900

    サインアップとログインを追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,35 @@
 import Link from "next/link";
+import { createClient } from "@/utils/supabase/server";
 
 export default async function Header() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
     <div className="navbar bg-neutral text-neutral-content">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Fjord Agent</a>
+        <Link href="/" className="btn btn-ghost text-xl">
+          Fjord Agent
+        </Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link href="/sign-up">サインアップ</Link>
-          </li>
-          <li>
-            <Link href="/sign-in">サインイン</Link>
-          </li>
+          {user ? (
+            <li>
+              <Link href="/logout">ログアウト</Link>
+            </li>
+          ) : (
+            <>
+              <li>
+                <Link href="/signup">ユーザー登録</Link>
+              </li>
+              <li>
+                <Link href="/login">ログイン</Link>
+              </li>
+            </>
+          )}
           <li>
             <details>
               <summary>ダミー</summary>
