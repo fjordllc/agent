262ac6af03bc7c9195904388a62158b358f832b5
kazuma-naka <kazuma.naka.01@gmail.com>
2024-12-29 20:03:35 -0500

    toBeInTheDocument を使用するための設定をする
    jest.setup.ts 内で jest-dom testing-library をインポートする
    
    types-react-dom をインストールする

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jest-environment-jsdom", // Ensures the correct environment for DOM testing
   transform: {
-    "^.+\\.[jt]sx?$": "babel-jest", // Transform .js, .jsx, .ts, .tsx files with babel-jest
+    "^.+\\.[jt]sx?$": "babel-jest", // Transform files with Babel
   },
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx"], // Recognize .jsx files
-  setupFiles: ["<rootDir>/jest.setup.js"], // Load custom setup file
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"], // Recognize these extensions
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // Ensure setup file is loaded after Jest environment is ready
 };
