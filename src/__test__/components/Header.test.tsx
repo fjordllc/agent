01a9a6b8a882ecb1a77fbd5569fd5c747868d0ff
kazuma-naka <kazuma.naka.01@gmail.com>
2025-01-04 11:38:57 -0500

    Header.test.tsx の１つのファイルに統合する

---
+++ src/__test__/components/Header.test.tsx
@@ -0,0 +1,42 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import Header from "../../components/Header";
+
+jest.mock("@/utils/supabase/server", () => ({
+  createClient: jest.fn().mockReturnValue({
+    auth: {
+      getUser: jest.fn(),
+    },
+  }),
+}));
+
+describe("Header コンポネントの描写テスト", () => {
+  const { createClient } = require("@/utils/supabase/server");
+  
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const renderHeaderWithUser = async (user: string | null) => {
+    createClient().auth.getUser.mockResolvedValueOnce({
+      data: { user },
+    });
+    render(await Header());
+  };
+
+  it("ユーザーが未ログインの場合の描写", async () => {
+    renderHeaderWithUser(null);
+    await waitFor(() => {
+      expect(screen.getAllByRole("listitem")).toHaveLength(5);
+      expect(screen.getByText("ユーザー登録"));
+      expect(screen.getByText("ログイン"));
+    });
+  });
+
+  it("ユーザーがログインの場合の描写", async () => {
+    renderHeaderWithUser("admin@test.com");
+    await waitFor(() => {
+      expect(screen.getAllByRole("listitem")).toHaveLength(4);
+      expect(screen.getByText("ログアウト"));
+    });
+  });
+});
