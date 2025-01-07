e25e5715db3ff361463a464d17e82a74e4932ce3
komagata <komagata@gmail.com>
2025-01-08 05:41:54 +0900

    フォーマット修正

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -30,7 +30,12 @@ export default function Auth() {
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <Image className="w-8 h-8 mr-2" src="pjord.svg" width="32" alt="logo" />
+          <Image
+            className="w-8 h-8 mr-2"
+            src="pjord.svg"
+            width="32"
+            alt="logo"
+          />
           Fjord Agent
         </div>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
