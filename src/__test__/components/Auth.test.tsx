c693420617c39c76a43cc916e42ffc6cbacc3af1
kazuma-naka <kazuma.naka.01@gmail.com>
2025-01-14 12:22:12 -0500

    コンフリクトの解消

--- src/__test__/components/Auth.test.tsx
+++ src/__test__/components/Auth.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import Auth from "@/components/Auth";
+import Auth from "../../components/Auth";
 
 test("Auth の初めてのテスト", () => {
   render(<Auth />);
