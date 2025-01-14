e2bbef5fe5945cc50478f4a2b20dda3d39bf6b83
kazuma-naka <kazuma.naka.01@gmail.com>
2025-01-14 12:23:12 -0500

    コンフリクトの解消

--- src/__test__/components/Auth.test.tsx
+++ src/__test__/components/Auth.test.tsx
@@ -1,8 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import Auth from "../../components/Auth";
 
-test("Auth の初めてのテスト", () => {
+test("Auth が正しく描写されているかチェックする", () => {
   render(<Auth />);
 
   expect(screen.getByText("ログイン")).toBeInTheDocument();
+  expect(
+    screen.getByRole("button", { name: "ログインメールを送る" })
+  ).toBeInTheDocument();
 });
