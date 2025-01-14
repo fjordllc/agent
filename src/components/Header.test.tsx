e5ced29a5d1359bacee783cd7dd909a59e2e4f91
kazuma-naka <kazuma.naka.01@gmail.com>
2025-01-14 13:07:56 -0500

    describe の組み立て方を変えて fail する部分も修正する
    data: {user: authCredential} のように user: を追加する

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -19,42 +19,47 @@ describe("Rendering test for the Header component", () => {
 
   const renderHeaderWithUser = async (authCredential?: AuthCredential) => {
     createClient().auth.getUser.mockResolvedValueOnce({
-      data: { authCredential },
+      data: { user: authCredential },
     });
     render(await Header());
   };
 
-  it("User is NOT logged in", async () => {
-    renderHeaderWithUser();
-    await waitFor(() => {
-      expect(screen.getAllByRole("listitem")).toHaveLength(5);
-      expect(screen.getByText("ユーザー登録"));
-      expect(screen.getByText("ログイン"));
+  describe("When user is logged in", () => {
+    test("Should display logout button", async () => {
+      const testUser: AuthCredential = {
+        email: "test@test.com",
+        password: "test",
+      };
+      renderHeaderWithUser(testUser);
+      await waitFor(() => {
+        expect(screen.getAllByRole("listitem")).toHaveLength(4);
+        expect(screen.getByText("ログアウト"));
+      });
     });
   });
 
-  it("User is logged in with valid email and password", async () => {
-    const testUser: AuthCredential = {
-      email: "test@test.com",
-      password: "test",
-    };
-    renderHeaderWithUser(testUser);
-    await waitFor(() => {
-      expect(screen.getAllByRole("listitem")).toHaveLength(4);
-      expect(screen.getByText("ログアウト"));
+  describe("When user is logged out", () => {
+    test("Should display signin and login button", async () => {
+      renderHeaderWithUser();
+      await waitFor(() => {
+        expect(screen.getAllByRole("listitem")).toHaveLength(5);
+        expect(screen.getByText("ユーザー登録"));
+        expect(screen.getByText("ログイン"));
+      });
     });
   });
 
-  it("auth.getUser() throws error", async () => {
-    createClient().auth.getUser.mockRejectedValueOnce({
-      error: "email_address_invalid",
-    });
-    render(await Header());
-
-    await waitFor(() => {
-      expect(screen.getByText("ユーザー登録")).toBeInTheDocument();
-      expect(screen.getByText("ログイン")).toBeInTheDocument();
-      expect(screen.queryByText("ログアウト")).not.toBeInTheDocument();
+  describe("When user is failed to log in", () => {
+    test("Should display error message", async () => {
+      createClient().auth.getUser.mockRejectedValueOnce({
+        error: "email_address_invalid",
+      });
+      render(await Header());
+      await waitFor(() => {
+        expect(screen.getByText("ユーザー登録")).toBeInTheDocument();
+        expect(screen.getByText("ログイン")).toBeInTheDocument();
+        expect(screen.queryByText("ログアウト")).not.toBeInTheDocument();
+      });
     });
   });
 });
