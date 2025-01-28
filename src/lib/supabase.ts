ab269963163128f041158ea716b236869ce24802
jyouma <163487418+Jyoumama@users.noreply.github.com>
2025-01-28 15:18:27 +0900

    機能追加: Supabaseの型定義とクライアント設定を更新

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./database.types";
 
-const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error(
+    `Supabase URL or Anon Key is not set in the environment variables. 
+    Ensure that NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY are correctly configured.`
+  );
+}
+
+const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
+
