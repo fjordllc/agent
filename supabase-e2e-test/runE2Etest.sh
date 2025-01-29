a9870f242ac052e7b8addc8c03e9411ca4629e76
kazuma-naka <kazuma.naka.01@gmail.com>
2025-01-28 19:29:03 -0500

    supabase を clone してローカルに構築する

--- supabase-e2e-test/runE2Etest.sh
+++ supabase-e2e-test/runE2Etest.sh
@@ -20,6 +20,30 @@ else
     # Pull the latest images
     echo "Pulling the latest Docker images..."
     docker compose pull
+
+    # Start the services (in detached mode)
+    echo "Starting services..."
+    docker compose up -d
+
+    echo "Services are up and running!"
+
+    echo "Applying migrations..."
+    MIGRATION_DIR="../../migrations"
+    for file in "$MIGRATION_DIR"/*.sql; do
+        echo "Applying migration: $file"
+        docker cp "$file" supabase-db:/tmp/$(basename "$file")
+        docker compose exec db psql -U postgres -d postgres -f /tmp/$(basename "$file")
+    done
+
+    echo "Migrations applied successfully!"
+
+    # Seed the database
+    echo "Seeding the database..."
+    docker cp ../../seed.test.sql supabase-db:/tmp/seed.test.sql
+    echo "Seeding the database..."
+    docker compose exec db psql -U postgres -d postgres -f /tmp/seed.test.sql
+    echo "Database seeding completed!"
+
 fi
 
 # Start the services (in detached mode)
@@ -27,21 +51,3 @@ echo "Starting services..."
 docker compose up -d
 
 echo "Services are up and running!"
-
-echo "Applying migrations..."
-MIGRATION_DIR="../../migrations"
-for file in "$MIGRATION_DIR"/*.sql; do
-    echo "Applying migration: $file"
-    docker cp "$file" supabase-db:/tmp/$(basename "$file")
-    docker compose exec db psql -U postgres -d postgres -f /tmp/$(basename "$file")
-done
-
-echo "Migrations applied successfully!"
-
-# Seed the database
-echo "Seeding the database..."
-docker cp ../../seed.test.sql supabase-db:/tmp/seed.test.sql
-echo "Seeding the database..."
-docker compose exec db psql -U postgres -d postgres -f /tmp/seed.test.sql
-
-echo "Database seeding completed!"
