1e554e19784116e6e2ab4c3f277782fc00cf54a8
jyouma <163487418+Jyoumama@users.noreply.github.com>
2025-02-03 05:32:15 +0900

    不要なマイグレーションファイルadd_public_read_access.sqlを削除しファイル名をTimestampで正しく取得

--- supabase/migrations/20250202120000_select_companies_policy.sql
+++ supabase/migrations/20250202120000_select_companies_policy.sql
@@ -4,3 +4,6 @@ FOR SELECT
 USING (true);
 
 GRANT SELECT ON ALL TABLES IN SCHEMA "public" TO authenticated;
+
+-- Public read access は不要なので削除
+drop policy if exists "Public read access" on "public"."companies";
---
+++ supabase/migrations/20250202153626_remove_policies.sql
@@ -0,0 +1,16 @@
+grant delete on table "public"."companies" to "authenticator";
+
+grant insert on table "public"."companies" to "authenticator";
+
+grant select on table "public"."companies" to "authenticator";
+
+grant update on table "public"."companies" to "authenticator";
+
+grant delete on table "public"."users" to "authenticator";
+
+grant insert on table "public"."users" to "authenticator";
+
+grant select on table "public"."users" to "authenticator";
+
+grant update on table "public"."users" to "authenticator";
+
