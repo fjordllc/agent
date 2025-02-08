213a46be1094fa0edd0406bb4d0cd9ad7b005a1c
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-08 18:45:09 -0500

    ドキュメント詳細のE2Eテストですべての値が表示されるか修正する

--- e2e/doclist.test.ts
+++ e2e/doclist.test.ts
@@ -4,6 +4,7 @@ import { withSupawright } from "supawright";
 import type { Database } from "@/lib/database.types";
 
 const test = withSupawright<Database, "public">(["public"]);
+const currentDate = "2025-02-08T23:00:38.455Z";
 
 describe("DocList E2E test", () => {
   test.beforeEach(async ({ supawright }) => {
@@ -19,6 +20,8 @@ describe("DocList E2E test", () => {
   test("should display test docs", async ({ page }) => {
     await page.goto("http://localhost:3000/");
     await page.getByRole("link", { name: "Docs" }).click();
-    await expect(page.getByText("This is a test document.")).toBeVisible();
+    await expect(
+      page.locator('p:has-text("This is a test document.")').first(),
+    ).toHaveText("This is a test document.");
   });
 });
