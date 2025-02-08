ea37c67006085c855c5d0019992f84176b5bc708
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-08 16:58:45 -0500

    ドキュメント詳細のE2Eテストを作成する

---
+++ e2e/doclist.test.ts
@@ -0,0 +1,31 @@
+import { describe } from "node:test";
+import { expect } from "@playwright/test";
+import { withSupawright } from "supawright";
+import type { Database } from "@/lib/database.types";
+
+const test = withSupawright<Database, "public">(["public"]);
+
+describe("DocList E2E test", () => {
+  test.beforeEach(async ({ supawright }) => {
+    await supawright.create("public", "docs", {
+      title: "Test Document",
+      body: "This is a test document.",
+      created_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+      user_id: 555,
+      last_updated_user_id: 555,
+    });
+  });
+  test("should display test docs", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+    await page.getByRole("link", { name: "Docs" }).click();
+    await expect(page.getByText("This is a test document.")).toBeVisible();
+  });
+  test("should display test docs in detail screen", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+    await page.getByRole("link", { name: "Docs" }).click();
+    await page.getByRole("link", { name: "Test Document" }).click();
+    await page.getByRole("link", { name: "Test Document" }).waitFor();
+    await expect(page.getByText("This is a test document.")).toBeVisible();
+  });
+});
