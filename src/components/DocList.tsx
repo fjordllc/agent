4e9279839f29588b47bcdbb7ba3f0014d31b34eb
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-08 15:04:26 -0500

    Dynamic に作成した詳細 Page に渡された id で title, body, user_id. last_updated_user_id, created_at, updated_at を取得する

--- src/components/DocList.tsx
+++ src/components/DocList.tsx
@@ -19,7 +19,21 @@ export default function DocList({ itemsPerPage }: DocsProps) {
         <ul>
           {docs.map((doc) => (
             <li key={doc.id}>
-              <Link href={`/docs/${doc.id}`}>{doc.title}</Link>
+              <Link
+                href={{
+                  pathname: `/docs/${doc.id}`,
+                  query: {
+                    title: doc.title,
+                    body: doc.body,
+                    created_at: doc.created_at,
+                    updated_at: doc.updated_at,
+                    user_id: doc.user_id,
+                    last_updated_user_id: doc.last_updated_user_id,
+                  },
+                }}
+              >
+                {doc.title}
+              </Link>
               <p>{doc.body}</p>
             </li>
           ))}
