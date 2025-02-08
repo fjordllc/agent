a9e302a5525cf0bf21828039afbf2f68b9759b96
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-08 15:05:26 -0500

    docs/[id] となるように修正

--- src/components/DocList.tsx
+++ src/components/DocList.tsx
@@ -19,21 +19,7 @@ export default function DocList({ itemsPerPage }: DocsProps) {
         <ul>
           {docs.map((doc) => (
             <li key={doc.id}>
-              <Link
-                href={{
-                  pathname: `/docs/${doc.id}`,
-                  query: {
-                    title: doc.title,
-                    body: doc.body,
-                    created_at: doc.created_at,
-                    updated_at: doc.updated_at,
-                    user_id: doc.user_id,
-                    last_updated_user_id: doc.last_updated_user_id,
-                  },
-                }}
-              >
-                {doc.title}
-              </Link>
+              <Link href={`/docs/${doc.id}`}>{doc.title}</Link>
               <p>{doc.body}</p>
             </li>
           ))}
