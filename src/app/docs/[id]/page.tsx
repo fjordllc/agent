302ca56ea2f8e64ee322a9cf23820f92d63c8031
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-08 15:04:26 -0500

    Dynamic に作成した詳細 Page に渡された id で title, body, user_id. last_updated_user_id, created_at, updated_at を取得する

--- src/app/docs/[id]/page.tsx
+++ src/app/docs/[id]/page.tsx
@@ -1,18 +1,37 @@
 import { notFound } from "next/navigation";
+import supabase from "@/lib/supabase";
 
-type Params = {
-  params: { id: string; title: string };
-};
+export default async function DocDetails({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { data: doc } = await supabase
+    .from("docs")
+    .select(
+      "title, body, user_id, last_updated_user_id, created_at, updated_at",
+    )
+    .eq("id", Number(params.id))
+    .single();
 
-export default async function DocDetails({ params }: Params) {
-  if (!params) {
-    notFound();
-  }
+  if (!doc) notFound();
 
   return (
     <div>
-      <h1>{params.id}</h1>
-      <p>{params.title}</p>
+      <h1>{doc.title}</h1>
+      <p>{doc.body}</p>
+      <p>
+        <strong>User ID:</strong> {doc.user_id}
+      </p>
+      <p>
+        <strong>Last Updated By:</strong> {doc.last_updated_user_id}
+      </p>
+      <p>
+        <strong>Created At:</strong> {new Date(doc.created_at).toLocaleString()}
+      </p>
+      <p>
+        <strong>Updated At:</strong> {new Date(doc.updated_at).toLocaleString()}
+      </p>
     </div>
   );
 }
