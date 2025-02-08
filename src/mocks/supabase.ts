15d0628b57531221b6c77e876e3f655c719042fb
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-07 23:46:31 -0500

    Docs のモック関数を mocks/supabase.ts に切り分ける

--- src/mocks/supabase.ts
+++ src/mocks/supabase.ts
@@ -5,9 +5,34 @@ export type AuthCredential = {
   password: string;
 };
 
+export type Document = {
+  id: string;
+  title: string;
+  body: string;
+  created_at: string;
+  updated_at: string;
+  user_id: string;
+  last_updated_user_id: string;
+};
+
 export const mockUserLoggedIn = async (user?: AuthCredential) => {
   const client = await createClient();
   (client.auth.getUser as jest.Mock).mockResolvedValueOnce({
     data: { user },
   });
 };
+<<<<<<< HEAD
+=======
+
+export const mockAuthError = async (error: AuthError) => {
+  const client = await createClient();
+  (client.auth.getUser as jest.Mock).mockRejectedValue(error);
+};
+
+export const mockDocs = async (docs: Document[]) => {
+  const client = await createClient();
+  (client.from as jest.Mock).mockReturnValue({
+    select: jest.fn().mockResolvedValueOnce({ data: docs, error: null }),
+  });
+};
+>>>>>>> d9d6952 (Docs のモック関数を mocks/supabase.ts に切り分ける)
