622a5c74bcf99ea22cbede9f43ea597fa890264d
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-09 10:54:06 -0500

    createdAt と updatedAt の static な時間を削除

--- e2e/doclist.test.ts
+++ e2e/doclist.test.ts
@@ -4,15 +4,12 @@ import { withSupawright } from "supawright";
 import type { Database } from "@/lib/database.types";
 
 const test = withSupawright<Database, "public">(["public"]);
-const currentDate = "2025-02-08T23:00:38.455Z";
 
 describe("DocList E2E test", () => {
   test.beforeEach(async ({ supawright }) => {
     await supawright.create("public", "docs", {
       title: "Test Document",
       body: "This is a test document.",
-      created_at: currentDate,
-      updated_at: currentDate,
       user_id: 555,
       last_updated_user_id: 555,
     });
@@ -23,14 +20,23 @@ describe("DocList E2E test", () => {
       await page.goto("http://localhost:3000/");
 
       await page.getByRole("link", { name: "Docs" }).click();
-      const docTitle = await page.getByRole("link", { name: "Test Document" }).first().textContent();
-    
+      const docTitle = await page
+        .getByRole("link", { name: "Test Document" })
+        .first()
+        .textContent();
+
       await page.getByRole("link", { name: "Test Document" }).first().click();
       await page.waitForLoadState("networkidle");
-      await expect(page.getByRole("heading", { name: docTitle! })).toBeVisible();
-      await expect(page.locator('p:has-text("This is a test document.")')).toBeVisible();
+      await expect(
+        page.getByRole("heading", { name: docTitle! }),
+      ).toBeVisible();
+      await expect(
+        page.locator('p:has-text("This is a test document.")'),
+      ).toBeVisible();
       await expect(page.locator('p:has-text("User ID:")')).toBeVisible();
-      await expect(page.locator('p:has-text("Last Updated By:")')).toBeVisible();
+      await expect(
+        page.locator('p:has-text("Last Updated By:")'),
+      ).toBeVisible();
       await expect(page.locator('p:has-text("Created At:")')).toBeVisible();
       await expect(page.locator('p:has-text("Updated At:")')).toBeVisible();
     });
