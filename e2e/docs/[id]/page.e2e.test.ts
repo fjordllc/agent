871a8f12d2467298365f3b48bde6d041d81af731
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-09 01:54:54 -0500

    E2E テストを追加する

--- e2e/docs/[id]/page.e2e.test.ts
+++ e2e/docs/[id]/page.e2e.test.ts
@@ -35,4 +35,22 @@ describe("DocList E2E test", () => {
       await expect(page.locator('p:has-text("Updated At:")')).toBeVisible();
     });
   });
+
+  test("delete doc in detail screen", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+    await page.getByRole("link", { name: "Docs" }).click();
+    await page.getByRole("link", { name: "Test Document" }).first().click();
+    await page.waitForLoadState("networkidle");
+
+    page.once("dialog", async (dialog) => {
+      console.log(`Dialog message: ${dialog.message()}`);
+      await dialog.accept();
+    });
+
+    await page.getByRole("button", { name: "ドキュメントを削除する" }).click();
+    await page.waitForLoadState("networkidle");
+    await expect(
+      page.locator('p:has-text("This is a test document.")'),
+    ).toHaveCount(0);
+  });
 });
