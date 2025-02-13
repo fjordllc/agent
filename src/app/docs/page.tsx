0714366113d43fd1e39409b94038f22251e32925
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-13 02:27:40 -0500

    ディレクトリやnits なものの修正

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 import Docs from "@/app/docs/_components/Docs";
 import SingleLayout from "@/components/layouts/SingleLayout";
+=======
+"use client";
+
+import Docs from "@/app/docs/_components/Docs";
+>>>>>>> e551e52 (ディレクトリやnits なものの修正)
 
 export default function DocsPage() {
   return (
