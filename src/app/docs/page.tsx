d74333a57df3123302dcb38540d644b1a5c18dee
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-13 10:36:49 -0500

    DocsPage を サーバーコンポネントにして Layout を無くす

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,9 +1,10 @@
 import Docs from "@/app/docs/_components/Docs";
+import SingleLayout from "@/components/layouts/SingleLayout";
 
 export default function DocsPage() {
   return (
-    <>
+    <SingleLayout>
       <Docs />
-    </>
+    </SingleLayout>
   );
 }
