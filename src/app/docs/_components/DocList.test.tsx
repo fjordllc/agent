8c59865d9c4a928660baf02990dcabb218789e44
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-14 01:02:33 -0500

    コンポネントテストのモックを next/navigation を追加する
    useRoute で詳細ページに遷移するため

--- src/app/docs/_components/DocList.test.tsx
+++ src/app/docs/_components/DocList.test.tsx
@@ -14,6 +14,24 @@ jest.mock("@/hooks/useDocs", () => ({
   useDocs: jest.fn(),
 }));
 
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(() => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+    prefetch: jest.fn(),
+    back: jest.fn(),
+    forward: jest.fn(),
+    refresh: jest.fn(),
+    pathname: "/",
+    query: {},
+    asPath: "/",
+  })),
+  usePathname: jest.fn(() => "/"),
+  useSearchParams: jest.fn(() => new URLSearchParams()),
+  notFound: jest.fn(),
+  redirect: jest.fn(),
+}));
+
 const mockUseDocs = (override = {}) => {
   return {
     docs: [
