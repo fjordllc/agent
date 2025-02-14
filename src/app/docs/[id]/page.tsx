80b332bd550a6d870b320a686d6abc72ca15fd92
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-14 11:24:13 -0500

    public.users から user_id を使って last_name, first_name を取得しようとしたが RLS を Diabled にしないといけない

--- src/app/docs/[id]/page.tsx
+++ src/app/docs/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import supabase from "@/lib/supabase";
 import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import SingleLayout from "@/components/layouts/SingleLayout";
 
 export default async function DocDetails({
   params,
@@ -9,41 +10,58 @@ export default async function DocDetails({
 }) {
   const { id } = await params;
 
-  const { data: doc, error } = await supabase
+  const { data: doc, error: docError } = await supabase
     .from("docs")
     .select("title,body, user_id, created_at, updated_at")
     .eq("id", Number(id))
     .single();
 
-  if (error) {
+  console.log(`doc: ${doc?.user_id}`);
+  const { data: user, error: userError } = await supabase
+    .from("users")
+    .select("last_name")
+    .eq("id", doc?.user_id ?? "")
+    .maybeSingle();
+
+  if (docError) {
     console.error(
-      `ドキュメントの取得に失敗しました。\n${error.code} ${error.message}`,
+      `ドキュメントの取得に失敗しました。\n${docError.code} ${docError.message}`,
+    );
+  }
+
+  if (userError) {
+    console.error(
+      `ユーザーの名前の取得に失敗しました。\n${userError.code} ${userError.message}`,
     );
   }
   if (!doc) notFound();
 
+  console.log(`user: ${user}`);
+
   return (
-    <Card className="p-6 max-w-2xl mx-auto my-6">
-      <CardHeader>
-        <div className="text-2xl font-bold">{doc.title}</div>
-      </CardHeader>
-      <CardContent>
-        <p className="mb-4 text-lg">{doc.body}</p>
-        <div className="space-y-2">
-          <p>
-            <span className="font-semibold text-gray-700">User:</span>{" "}
-            {doc.user_id}
-          </p>
-          <p>
-            <span className="font-semibold text-gray-700">Created At:</span>{" "}
-            {new Date(doc.created_at).toLocaleString()}
-          </p>
-          <p>
-            <span className="font-semibold text-gray-700">Updated At:</span>{" "}
-            {new Date(doc.updated_at).toLocaleString()}
-          </p>
-        </div>
-      </CardContent>
-    </Card>
+    <SingleLayout>
+      <Card className="p-6 max-w-2xl mx-auto my-6">
+        <CardHeader>
+          <div className="text-2xl font-bold">{doc.title}</div>
+        </CardHeader>
+        <CardContent>
+          <p className="mb-4 text-lg">{doc.body}</p>
+          <div className="space-y-2">
+            <p>
+              <span className="font-semibold text-gray-700">User:</span>{" "}
+              {user?.last_name}
+            </p>
+            <p>
+              <span className="font-semibold text-gray-700">Created At:</span>{" "}
+              {new Date(doc.created_at).toLocaleString()}
+            </p>
+            <p>
+              <span className="font-semibold text-gray-700">Updated At:</span>{" "}
+              {new Date(doc.updated_at).toLocaleString()}
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    </SingleLayout>
   );
 }
