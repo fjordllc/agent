e28ad7905c236079bc77b49a88705812f30f94ef
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-14 11:24:13 -0500

    public.users から user_id を使って last_name, first_name を取得しようとしたが RLS を Diabled にしないといけない

--- supabase/database.types.ts
+++ supabase/database.types.ts
@@ -4,140 +4,138 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export type Database = {
   graphql_public: {
     Tables: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
       graphql: {
         Args: {
-          operationName?: string
-          query?: string
-          variables?: Json
-          extensions?: Json
-        }
-        Returns: Json
-      }
-    }
+          operationName?: string;
+          query?: string;
+          variables?: Json;
+          extensions?: Json;
+        };
+        Returns: Json;
+      };
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
   public: {
     Tables: {
       companies: {
         Row: {
-          created_at: string
-          id: number
-          memo: string | null
-          name: string
-          website: string | null
-        }
+          created_at: string;
+          id: number;
+          memo: string | null;
+          name: string;
+          website: string | null;
+        };
         Insert: {
-          created_at?: string
-          id?: number
-          memo?: string | null
-          name: string
-          website?: string | null
-        }
+          created_at?: string;
+          id?: number;
+          memo?: string | null;
+          name: string;
+          website?: string | null;
+        };
         Update: {
-          created_at?: string
-          id?: number
-          memo?: string | null
-          name?: string
-          website?: string | null
-        }
-        Relationships: []
-      }
+          created_at?: string;
+          id?: number;
+          memo?: string | null;
+          name?: string;
+          website?: string | null;
+        };
+        Relationships: [];
+      };
       docs: {
         Row: {
-          body: string
-          created_at: string | null
-          id: number
-          last_updated_user_id: number
-          title: string
-          updated_at: string | null
-          user_id: number
-        }
+          id: number;
+          title: string;
+          body: string;
+          created_at: string;
+          updated_at: string;
+          user_id: string;
+          last_updated_user_id: string;
+        };
         Insert: {
-          body: string
-          created_at?: string | null
-          id?: number
-          last_updated_user_id: number
-          title: string
-          updated_at?: string | null
-          user_id: number
-        }
+          title: string;
+          body: string;
+          created_at?: string;
+          updated_at?: string;
+          user_id: string;
+          last_updated_user_id: string;
+        };
         Update: {
-          body?: string
-          created_at?: string | null
-          id?: number
-          last_updated_user_id?: number
-          title?: string
-          updated_at?: string | null
-          user_id?: number
-        }
-        Relationships: []
-      }
+          title?: string;
+          body?: string;
+          created_at?: string;
+          updated_at?: string;
+          user_id?: string;
+          last_updated_user_id?: string;
+        };
+        Relationships: [];
+      };
       users: {
         Row: {
-          date_of_birth: string | null
-          email: string
-          first_name: string | null
-          first_name_kana: string | null
-          id: string
-          last_name: string | null
-          last_name_kana: string | null
-          role: string | null
-        }
+          date_of_birth: string | null;
+          email: string;
+          first_name: string | null;
+          first_name_kana: string | null;
+          id: string;
+          last_name: string | null;
+          last_name_kana: string | null;
+          role: string | null;
+        };
         Insert: {
-          date_of_birth?: string | null
-          email: string
-          first_name?: string | null
-          first_name_kana?: string | null
-          id: string
-          last_name?: string | null
-          last_name_kana?: string | null
-          role?: string | null
-        }
+          date_of_birth?: string | null;
+          email: string;
+          first_name?: string | null;
+          first_name_kana?: string | null;
+          id: string;
+          last_name?: string | null;
+          last_name_kana?: string | null;
+          role?: string | null;
+        };
         Update: {
-          date_of_birth?: string | null
-          email?: string
-          first_name?: string | null
-          first_name_kana?: string | null
-          id?: string
-          last_name?: string | null
-          last_name_kana?: string | null
-          role?: string | null
-        }
-        Relationships: []
-      }
-    }
+          date_of_birth?: string | null;
+          email?: string;
+          first_name?: string | null;
+          first_name_kana?: string | null;
+          id?: string;
+          last_name?: string | null;
+          last_name_kana?: string | null;
+          role?: string | null;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -150,7 +148,7 @@ export type Tables<
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+      Row: infer R;
     }
     ? R
     : never
@@ -158,11 +156,11 @@ export type Tables<
         PublicSchema["Views"])
     ? (PublicSchema["Tables"] &
         PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
+        Row: infer R;
       }
       ? R
       : never
-    : never
+    : never;
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -173,17 +171,17 @@ export type TablesInsert<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+      Insert: infer I;
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
+        Insert: infer I;
       }
       ? I
       : never
-    : never
+    : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -194,17 +192,17 @@ export type TablesUpdate<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+      Update: infer U;
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
+        Update: infer U;
       }
       ? U
       : never
-    : never
+    : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -217,14 +215,14 @@ export type Enums<
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+    : never;
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof PublicSchema["CompositeTypes"]
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
@@ -232,5 +230,4 @@ export type CompositeTypes<
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
     ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
-
+    : never;
