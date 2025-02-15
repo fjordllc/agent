ce938e9e98b36fcae826f949a4c6414b90985b2f
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-15 05:30:48 -0500

    e2e テストを serial で run する

--- e2e/docs/[id]/page.e2e.test.ts
+++ e2e/docs/[id]/page.e2e.test.ts
@@ -6,17 +6,38 @@ import type { Database } from "@/lib/database.types";
 const test = withSupawright<Database, "public">(["public"]);
 
 describe("DocList E2E test", () => {
-  test.beforeEach(async ({ supawright }) => {
-    await supawright.create("public", "docs", {
-      title: "Test Document",
-      body: "This is a test document.",
-      user_id: "acfeb157-6c90-4d70-ad96-1d6361c1874e",
-      last_updated_user_id: "acfeb157-6c90-4d70-ad96-1d6361c1874e",
+  test.describe.serial("document detail", () => {
+    test.beforeEach(async ({ supawright }) => {
+      await supawright.create("public", "docs", {
+        title: "Test Document",
+        body: "This is a test document.",
+        user_id: "acfeb157-6c90-4d70-ad96-1d6361c1874e",
+        last_updated_user_id: "acfeb157-6c90-4d70-ad96-1d6361c1874e",
+      });
     });
-  });
 
-  describe("document detail", () => {
-    test("should display test docs in detail screen", async ({ page }) => {
+    describe("document detail", () => {
+      test("should display test docs in detail screen", async ({ page }) => {
+        await page.goto("http://localhost:3000/");
+
+        await page.getByRole("link", { name: "Docs" }).click();
+        await page.waitForLoadState("networkidle");
+        await expect(page.getByText("Test Document")).toBeVisible();
+        await page.getByText("Test Document").click();
+        await page.waitForLoadState("networkidle");
+        await expect(
+          page.locator('div:has-text("Test Document")').first(),
+        ).toBeVisible();
+        await expect(
+          page.locator('p:has-text("This is a test document.")'),
+        ).toBeVisible();
+        await expect(page.locator('p:has-text("User:")')).toBeVisible();
+        await expect(page.locator('p:has-text("Created At:")')).toBeVisible();
+        await expect(page.locator('p:has-text("Updated At:")')).toBeVisible();
+      });
+    });
+
+    test("delete doc in detail screen", async ({ page }) => {
       await page.goto("http://localhost:3000/");
 
       await page.getByRole("link", { name: "Docs" }).click();
@@ -24,33 +45,17 @@ describe("DocList E2E test", () => {
       await expect(page.getByText("Test Document")).toBeVisible();
       await page.getByText("Test Document").click();
       await page.waitForLoadState("networkidle");
-      await expect(
-        page.locator('div:has-text("Test Document")').first(),
-      ).toBeVisible();
+
+      page.once("dialog", async (dialog) => {
+        console.log(`Dialog message: ${dialog.message()}`);
+        await dialog.accept();
+      });
+
+      await page.getByRole("button", { name: "削除する" }).click();
+      await page.waitForLoadState("networkidle");
       await expect(
         page.locator('p:has-text("This is a test document.")'),
-      ).toBeVisible();
-      await expect(page.locator('p:has-text("User:")')).toBeVisible();
-      await expect(page.locator('p:has-text("Created At:")')).toBeVisible();
-      await expect(page.locator('p:has-text("Updated At:")')).toBeVisible();
+      ).toHaveCount(0);
     });
-  });
-
-  test("delete doc in detail screen", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
-    await page.getByRole("link", { name: "Docs" }).click();
-    await page.getByRole("link", { name: "Test Document" }).first().click();
-    await page.waitForLoadState("networkidle");
-
-    page.once("dialog", async (dialog) => {
-      console.log(`Dialog message: ${dialog.message()}`);
-      await dialog.accept();
-    });
-
-    await page.getByRole("button", { name: "ドキュメントを削除する" }).click();
-    await page.waitForLoadState("networkidle");
-    await expect(
-      page.locator('p:has-text("This is a test document.")'),
-    ).toHaveCount(0);
   });
 });
