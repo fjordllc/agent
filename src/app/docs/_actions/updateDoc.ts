a6d83e734cf9b7b7169fd55df906362644828d01
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-20 09:57:56 -0500

    cast を使わずエラーハンドリングする

--- src/app/docs/_actions/updateDoc.ts
+++ src/app/docs/_actions/updateDoc.ts
@@ -4,12 +4,22 @@ import { createClient } from "@/lib/supabaseServer";
 import { redirect } from "next/navigation";
 
 export async function updateDoc(formData: FormData) {
-  const title = formData.get("title") as string;
-  const body = formData.get("body") as string;
-  const id = formData.get("id") as string;
+  const titleRaw = formData.get("title");
+  const bodyRaw = formData.get("body");
+  const idRaw = formData.get("id");
 
+  if (
+    typeof titleRaw !== "string" ||
+    typeof bodyRaw !== "string" ||
+    typeof idRaw !== "string"
+  ) {
+    throw new Error("Invalid form data");
+  }
   const supabase = await createClient();
-  await supabase.from("docs").update({ title, body }).eq("id", Number(id));
+  await supabase
+    .from("docs")
+    .update({ titleRaw, bodyRaw })
+    .eq("id", Number(idRaw));
 
-  redirect(`/docs/${id}`);
+  redirect(`/docs/${idRaw}`);
 }
