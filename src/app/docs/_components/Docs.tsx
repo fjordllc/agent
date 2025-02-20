1a6c1ed2a238bc09ca92a1534ff41526a10833a6
kazuma-naka <kazuma.naka.01@gmail.com>
2025-02-20 09:44:54 -0500

    nits の部分を修正

--- src/app/docs/_components/Docs.tsx
+++ src/app/docs/_components/Docs.tsx
@@ -2,9 +2,8 @@ import DocList from "./DocList";
 
 export default function Docs() {
   return (
-    <div style={{ padding: "20px" }}>
+    <div className="p-20px">
       <h1>Docs</h1>
-       {/* BootCamp のドキュメント一覧は毎ページ 20 アイテム*/}
       <DocList itemsPerPage={20} />
     </div>
   );
