b11d46e490e8c77d9cac24cc4942eb5c088f0de0
kazuma-naka <kazuma.naka.01@gmail.com>
2025-03-04 21:11:02 -0500

    app/docs/page.tsx のコンフリクトを解消

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 import Docs from "@/app/docs/_components/Docs";
 import SingleLayout from "@/components/layouts/SingleLayout";
-=======
-"use client";
-
-import Docs from "@/app/docs/_components/Docs";
->>>>>>> e551e52 (ディレクトリやnits なものの修正)
 
 export default function DocsPage() {
   return (
